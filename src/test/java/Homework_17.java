9222ea2ab14e6b35a6ec68ac2ec19cce1c5793f0
afaabbasli <afa.abbasli@testpro.io>
2023-04-26 21:08:53 -0700

    Created a test case, registrationNavigation() using @Test annotation.
    - Navigated to "https://bbb.testpro.io/".
    
    -Verified that you are redirected to Registration page using Assert method.

---
+++ src/test/java/Homework_17.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class Homework_17 {
+    @Test
+        public void addSongToPlaylist() throws InterruptedException {
+        String newSongAddedNotificationText="Added 1 song into";
+    }
+}
